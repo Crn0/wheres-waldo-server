f3bac4e51225aa1e42403d2d4ef12f20d136f169
crno <118383162+Crn0@users.noreply.github.com.>
2025-01-26 17:03:58 +0800

    build: add errors parameter

--- src/errors/api-error.js
+++ src/errors/api-error.js
@@ -1,8 +1,14 @@
 import BaseError from "./base-error.js";
 
 class APIError extends BaseError {
-  constructor(message, httpCode, name = "API Error", isOperational = true) {
-    super(message, httpCode, name, isOperational);
+  constructor(
+    message,
+    httpCode,
+    name = "API Error",
+    errors = null,
+    isOperational = true
+  ) {
+    super(message, httpCode, name, errors, isOperational);
   }
 }
 
