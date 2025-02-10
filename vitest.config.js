44cc6230e7668b5f40b8a38a207f0af139f71dd1
crno <118383162+Crn0@users.noreply.github.com.>
2025-02-10 15:42:19 +0800

    build: include all test

--- vitest.config.js
+++ vitest.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ["src/test/**/*.test.js"],
+    include: ["src/test/**/*.test.js", "src/**/*.test.js"],
     setupFiles: "./test/setup-db.js",
     clearMocks: true,
     threads: false,
