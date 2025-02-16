7bd2cd0df87e417c8e68d8f0ffab83715d03f1ef
crno <118383162+Crn0@users.noreply.github.com.>
2025-02-16 17:18:00 +0800

    fix(cookie-config): chain env property

--- src/configs/cookie-config.js
+++ src/configs/cookie-config.js
@@ -3,8 +3,8 @@ import constants from "../constants/index.js";
 const cookieConfig = {
   maxAge: 24 * 60 * 60 * 1000, // one day
   httpOnly: true,
-  secure: constants.NODE_ENV === "prod",
-  sameSite: constants.NODE_ENV === "prod" ? "none" : "lax",
+  secure: constants.env.NODE_ENV === "prod",
+  sameSite: constants.env.NODE_ENV === "prod" ? "none" : "lax",
 };
 
 export default cookieConfig;
